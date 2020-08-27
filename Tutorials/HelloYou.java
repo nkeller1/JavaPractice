0b03f224a52acb2a407affd93e8db6061e40a6a2
Nathan Keller <keller.nathan@gmail.com>
2020-08-27 12:59:37 -0600

    add comments to hello

---
+++ Tutorials/HelloYou.java
@@ -0,0 +1,11 @@
+// declates a class, public vs private
+public class HelloYou {
+  // static = we do not need to create an object
+  // void - it does not return a value
+  // main- it is the method name, the entry point where the JVM runs the program
+  // String[args] - used for command linbe arguements passed as strings
+  public static void main (String[] args) {
+  // println printes the string
+    System.out.println("Hello Nathan");
+  }
+}
