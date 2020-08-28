069c9729fcf60f65ecfbf21b9565c9aa5bd0bda1
Nathan Keller <keller.nathan@gmail.com>
2020-08-28 10:51:09 -0600

    Tutorial for int variables that counts comments

---
+++ Tutorials/CountComment.java
@@ -0,0 +1,12 @@
+//This is the class declaration
+public class CountComment {
+  //This is the main method that runs when you compile
+	public static void main(String[] args) {
+    //This is defines an int variable 
+    int numComments = 6;
+    //This prints the variable
+    System.out.println(numComments);
+	}
+  //This is the end of the class
+}
+//This is outside the class
