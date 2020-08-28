6145eab36423ac4b79c440110284356d67c3ffbf
Nathan Keller <keller.nathan@gmail.com>
2020-08-28 10:57:56 -0600

    Tutorial for Char datatype created.

---
+++ Tutorials/Char.java
@@ -0,0 +1,8 @@
+public class Char {
+	public static void main(String[] args) {
+    // char datatype can take a single letter.
+    // MUST be enclosed in single quotes only   
+    char expectedGrade = 'B';
+    System.out.println(expectedGrade);
+	}
+}
