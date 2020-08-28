198e90df90e39b62f089962ac48d88e456d609c4
Nathan Keller <keller.nathan@gmail.com>
2020-08-28 10:46:30 -0600

    variables

---
+++ Tutorials/Creator.java
@@ -0,0 +1,10 @@
+public class Creator {
+	public static void main(String[] args) {
+    // declares the variable name as a string
+    String name = "James Gosling";
+    // declares the variable yearsCreated as a int
+    int yearCreated = 1995;
+    System.out.println(name);
+    System.out.println(yearCreated);
+	}
+}
