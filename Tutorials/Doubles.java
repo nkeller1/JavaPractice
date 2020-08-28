adf022b5ff8007ff9dc71dd926de4d06ebb69bef
Nathan Keller <keller.nathan@gmail.com>
2020-08-28 10:54:28 -0600

    Complete tutorial for the doubles data type. flot for ruby

---
+++ Tutorials/Doubles.java
@@ -0,0 +1,7 @@
+public class Doubles {
+	public static void main(String[] args) {
+    // doubles are floats or VERY large numbers. They take up more space
+    double androidShare = 81.7;
+    System.out.println(androidShare);
+	}
+}
