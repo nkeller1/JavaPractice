03341bbd8b3c063bc1f9849beb945b1a05be20ef
Nathan Keller <keller.nathan@gmail.com>
2020-08-28 11:05:14 -0600

    Add file to fix wrong datatypes.

---
+++ Tutorials/WrongDataTypes.java
@@ -0,0 +1,10 @@
+// the below varaibles have the wrong datatypes. Fix them!
+public class WrongDataTypes {
+	public static void main(String[] args) {
+		String year = 2001;
+    double title = "Shrek";
+    int genre = 'C';
+    boolean runtime = 1.58;
+    char isPG = true;
+	}
+}
