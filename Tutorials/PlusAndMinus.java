d34ee42c94ea89f71a8a63aaaa6d1be60b92e8dd
Nathan Keller <keller.nathan@gmail.com>
2020-08-30 16:43:18 -0600

    Add more variables to plus and minus

--- Tutorials/PlusAndMinus.java
+++ Tutorials/PlusAndMinus.java
@@ -3,6 +3,8 @@ public class PlusAndMinus {
 		int zebrasInZoo = 8;
     int giraffesInZoo = 4;
     int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    int numZebrasAfterTrade = zebrasInZoo - 2;
     System.out.println(animalsInZoo);
+    System.out.println(numZebrasAfterTrade);
 	}
 }
