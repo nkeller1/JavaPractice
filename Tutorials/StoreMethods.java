48de5300edfa24a28f5b3db4dc0da072185251bd
Nathan Keller <keller.nathan@gmail.com>
2020-09-01 10:56:37 -0600

    Add increasePrice method to Store

--- Tutorials/StoreMethods.java
+++ Tutorials/StoreMethods.java
@@ -1,10 +1,11 @@
 public class StoreMethods {
   // instance fields
   String productType;
-
+  double price;
   // constructor method
-  public StoreMethods(String product) {
+  public StoreMethods(String product, double initialPrice) {
     productType = product;
+    price = initialPrice;
   }
 
   // advertise method
@@ -20,12 +21,18 @@ public class StoreMethods {
     System.out.println("Welcome to the store, " + customer + "!");
   }
 
+  // increase price method
+  public void increasePrice(double priceToAdd){
+    double newPrice = price + priceToAdd;
+    price = newPrice;
+  }
+
   // main method
   public static void main(String[] args) {
-    StoreMethods lemonadeStand = new StoreMethods("Lemonade");
-    lemonadeStand.advertise();
-    lemonadeStand.advertise();
+    StoreMethods lemonadeStand = new StoreMethods("Lemonade", 3.75);
     lemonadeStand.advertise();
     lemonadeStand.greetCustomer("Nathan");
+    lemonadeStand.increasePrice(1.5);
+    System.out.println(lemonadeStand.price);
   }
 }
