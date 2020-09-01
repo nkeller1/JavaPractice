b11bc49dbf7a7f57ad3169a24a948252e110a517
Nathan Keller <keller.nathan@gmail.com>
2020-09-01 10:41:26 -0600

    Call Advertise Method multiple times.

--- Tutorials/StoreMethods.java
+++ Tutorials/StoreMethods.java
@@ -3,19 +3,22 @@ public class StoreMethods {
   String productType;
 
   // constructor method
-  public Store(String product) {
+  public StoreMethods(String product) {
     productType = product;
   }
 
   // advertise method
   // public means other classes can access the method
-  // void means there is no specific output from the method. 
+  // void means there is no specific output from the method.
   public void advertise(){
     System.out.println("Come spend some money!");
     System.out.println("Selling " + productType + "!");
   }
   // main method
   public static void main(String[] args) {
-
+    StoreMethods lemonadeStand = new StoreMethods("Lemonade");
+    lemonadeStand.advertise();
+    lemonadeStand.advertise();
+    lemonadeStand.advertise();
   }
 }
