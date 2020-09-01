98713a769dbdfcb62259daa653df4917437a24b9
Nathan Keller <keller.nathan@gmail.com>
2020-09-01 10:35:02 -0600

    Methods

---
+++ Tutorials/StoreMethods.java
@@ -0,0 +1,17 @@
+public class StoreMethods {
+  // instance fields
+  String productType;
+
+  // constructor method
+  public Store(String product) {
+    productType = product;
+  }
+
+  // advertise method
+
+  // main method
+  // public means other classes can access the method
+  public static void main(String[] args) {
+
+  }
+}
