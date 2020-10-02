b1c72cee12417249e5eed8663623b7f81bf7c559
Nathan Keller <keller.nathan@gmail.com>
2020-10-02 14:32:57 -0600

    if else

--- Tutorials/Order.java
+++ Tutorials/Order.java
@@ -2,10 +2,12 @@ public class Order {
 
     public static void main(String[] args) {
 
-        double itemCost = 30.99;
+        boolean isFilled = false;
 
-        if(itemCost > 24.00){
-            System.out.println("High Value Item");
+        if(isFilled){
+            System.out.println("Shipping");
+        } else {
+            System.out.println("Order Not READY");
         }
     }
 }
