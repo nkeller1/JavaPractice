4ba219f5cdb9670a9bbb852296c5402b655952fe
Nathan Keller <keller.nathan@gmail.com>
2020-10-02 14:41:38 -0600

    calculate a shipping cost

--- Tutorials/Order.java
+++ Tutorials/Order.java
@@ -1,13 +1,42 @@
 public class Order {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
+
+    public Order(boolean filled, double cost, String shippingMethod) {
+        if (cost > 24.00) {
+            System.out.println("High value item!");
+        }
+        isFilled = filled;
+        billAmount = cost;
+        shipping = shippingMethod;
+    }
+
+    public void ship() {
+        if (isFilled) {
+            System.out.println("Shipping");
+            System.out.println("Shipping cost: " + calculateShipping());
+        } else {
+            System.out.println("Order not ready");
+        }
+    }
+
+    public double calculateShipping() {
+        if (shipping.equals("Regular")){
+            return 0;
+        } else if (shipping.equals("Express")){
+            return 1.75;
+        } else {
+            return .50;
+        }
+    }
 
     public static void main(String[] args) {
+        // do not alter the main method!
+        Order book = new Order(true, 9.99, "Express");
+        Order chemistrySet = new Order(false, 72.50, "Regular");
 
-        boolean isFilled = false;
-
-        if(isFilled){
-            System.out.println("Shipping");
-        } else {
-            System.out.println("Order Not READY");
-        }
+        book.ship();
+        chemistrySet.ship();
     }
 }
