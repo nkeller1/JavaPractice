e621d173b07cc2019992efec844283a0c12eaf46
Nathan Keller <keller.nathan@gmail.com>
2020-10-10 13:09:38 -0600

    utlizie the .size() method

--- Tutorials/ToDos.java
+++ Tutorials/ToDos.java
@@ -4,16 +4,29 @@ public class ToDos {
 
     public static void main(String[] args) {
 
-        ArrayList<String> toDoList = new ArrayList<String>();
-        String toDo1 = "Water plants";
-        // Add more to-dos here:
-        String toDo2 = "Clean Rufio's Box";
-        String toDo3 = "Go to PetCo";
-        // Add to-dos to toDoList
-        toDoList.add(toDo1);
-        toDoList.add(toDo2);
-        toDoList.add(toDo3);
+        // Sherlock
+        ArrayList<String> sherlocksToDos = new ArrayList<String>();
 
-        System.out.println(toDoList);
+        sherlocksToDos.add("visit the crime scene");
+        sherlocksToDos.add("play violin");
+        sherlocksToDos.add("interview suspects");
+        sherlocksToDos.add("solve the case");
+        sherlocksToDos.add("apprehend the criminal");
+
+        // Poirot
+        ArrayList<String> poirotsToDos = new ArrayList<String>();
+
+        poirotsToDos.add("visit the crime scene");
+        poirotsToDos.add("interview suspects");
+        poirotsToDos.add("let the little grey cells do their work");
+        poirotsToDos.add("trim mustache");
+        poirotsToDos.add("call all suspects together");
+        poirotsToDos.add("reveal the truth of the crime");
+
+        // Print the size of each ArrayList below:
+        System.out.println(sherlocksToDos.size());
+        System.out.println(poirotsToDos.size());
+
+        System.out.println("Poirot has a longer to do list.");
     }
 }
