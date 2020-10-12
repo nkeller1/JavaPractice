b2457c130a2a4f8e11b2cc5475ffc08d19698551
Nathan Keller <keller.nathan@gmail.com>
2020-10-12 12:18:09 -0600

    implement for each loop to iterate and get the most expensive item

--- Tutorials/MostExpensive.java
+++ Tutorials/MostExpensive.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class MostExpensive {
+import java.util.ArrayList;
+
+class MostExpensive {
+
+    public static void main(String[] args) {
+
+        ArrayList<Double> expenses = new ArrayList<Double>();
+        expenses.add(74.46);
+        expenses.add(63.99);
+        expenses.add(10.57);
+        expenses.add(81.37);
+
+        double mostExpensive = 0;
+
+        // Iterate over expenses
+        for (double expense : expenses) {
+            if (expense > mostExpensive) {
+                mostExpensive = expense;
+            }
+        }
+
+        System.out.println(mostExpensive);
+
+    }
+
 }
