8dff7f3ae0be7632c99d228be28521dea19af065
Nathan Keller <keller.nathan@gmail.com>
2020-10-12 12:46:21 -0600

    write code to combine names with the concat mathod

--- Tutorials/CombineNames.java
+++ Tutorials/CombineNames.java
@@ -1,2 +1,15 @@
+
 public class CombineNames {
+
+    public static void main(String[] args) {
+
+        // Add your first names:
+        String firstName = "Nathan ";
+        String lastName = "Keller";
+
+        // What's the full name?
+        System.out.println(firstName.concat(lastName));
+
+    }
+
 }
