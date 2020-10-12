f8e41236f8344b1c11cb1d24722d57c2abff11e6
Nathan Keller <keller.nathan@gmail.com>
2020-10-12 11:21:33 -0600

    refactor while loop with counter to for loop

--- Tutorials/Coffee.java
+++ Tutorials/Coffee.java
@@ -3,11 +3,16 @@ class Coffee {
     public static void main(String[] args) {
 
         // initialize cupsOfCoffee
-        int cupsOfCoffee = 1;
-        // add while loop with counter
-        while (cupsOfCoffee <= 100){
+//        int cupsOfCoffee = 1;
+//        // add while loop with counter
+//        while (cupsOfCoffee <= 100){
+//            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+//            cupsOfCoffee++;
+//        }
+
+//        As a for loop
+        for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++){
             System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
-            cupsOfCoffee++;
         }
     }
 }
