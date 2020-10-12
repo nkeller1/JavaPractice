75dca2dfa82321652ebc54f2e5f052403e5ed512
Nathan Keller <keller.nathan@gmail.com>
2020-10-12 12:11:22 -0600

    iterate over an array of doubles and total them

--- Tutorials/CalculateTotal.java
+++ Tutorials/CalculateTotal.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class CalculateTotal {
+import java.util.ArrayList;
+
+class CalculateTotal {
+
+    public static void main(String[] args) {
+
+        ArrayList<Double> expenses = new ArrayList<Double>();
+        expenses.add(74.46);
+        expenses.add(63.99);
+        expenses.add(10.57);
+        expenses.add(81.37);
+
+        double total = 0;
+
+        // Iterate over expenses
+        for (int i = 0; i < expenses.size(); i++){
+            double item = expenses.get(i);
+            total = total + item;
+        }
+
+        System.out.println(total);
+
+    }
+
 }
