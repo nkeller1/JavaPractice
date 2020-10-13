44319c316a25c0b507c350ddb1ab26bcd42a5e3c
Nathan Keller <keller.nathan@gmail.com>
2020-10-13 10:54:22 -0600

    wrote some code using .equals()

--- Tutorials/Password.java
+++ Tutorials/Password.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Password {
+public class Password {
+
+    public static void main(String[] args) {
+
+        String password = "correcthorsebatterystaple";
+        String wrongPassword = "thisiswrong";
+        String correctPassword = "correcthorsebatterystaple";
+        // Write the code:
+        System.out.println(password.equals(wrongPassword));
+        System.out.println(password.equals(correctPassword));
+    }
+
 }
